909c0fb99b39008c7907d7a8320ff6a0345ba108
Nabeel <khankumail298@gmail.com>
2021-09-12 12:18:40 +0500

    commeting previous comment and now working on OOP

--- code.js
+++ code.js
@@ -1,45 +1,45 @@
-//full documentation is provided in my oneNote
+// //full documentation is provided in my oneNote
 
-//---------------------------------------const vs let vs var
-function tempVAR(a){
-    console.log(input) //will work b/c of hoisted property of var (but since no assigment so undefined)
-    if(a=='123'){
-        var input='abcdefgh'
-    }else{
-        var input='1123123123'
-    }
-    console.log(input) //will work b/c of hoisted property of var
+// //---------------------------------------const vs let vs var
+// function tempVAR(a){
+//     console.log(input) //will work b/c of hoisted property of var (but since no assigment so undefined)
+//     if(a=='123'){
+//         var input='abcdefgh'
+//     }else{
+//         var input='1123123123'
+//     }
+//     console.log(input) //will work b/c of hoisted property of var
     
-}
-// tempVAR('123')  //call this and check output
+// }
+// // tempVAR('123')  //call this and check output
 
-function tempLET(a){
-    if(a=='123'){
-        let input='abcdefgh'
-    }else{
-        let input='1123123123'
-    }
-    console.log(input) //will work b/c of hoisted property of var
-}
-//tempLET('123') //call this and check output
+// function tempLET(a){
+//     if(a=='123'){
+//         let input='abcdefgh'
+//     }else{
+//         let input='1123123123'
+//     }
+//     console.log(input) //will work b/c of hoisted property of var
+// }
+// //tempLET('123') //call this and check output
 
-//-----------------------------------------arrow functions
-//typical function 
-let multiple20=function (){
-    return 20;
-}
+// //-----------------------------------------arrow functions
+// //typical function 
+// let multiple20=function (){
+//     return 20;
+// }
 
-//arrow function
-//eg1
-const multiple10= a => 10*a;  //this is arrow function
-console.log(multiple10(2))
+// //arrow function
+// //eg1
+// const multiple10= a => 10*a;  //this is arrow function
+// console.log(multiple10(2))
 
-//eg2
-const squareSum= (a,b) =>{
-    a*=a;
-    b*=b;
-    return a+b;
-}
-console.log(squareSum(1,3));
+// //eg2
+// const squareSum= (a,b) =>{
+//     a*=a;
+//     b*=b;
+//     return a+b;
+// }
+// console.log(squareSum(1,3));
 
 //OOP Cooncepts
